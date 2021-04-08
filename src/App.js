cfd92a3c3354b44199f9ecc9198a62f7fd7d093d
Cenoroid <almostwar@gmail.com>
2021-04-09 02:55:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import io from "socket.io-client";
+import "./App.css";
+import trumpet from "./audio/trumpet.mp3";
+import stuck from "./audio/stuck.mp3";
+import AWS from "aws-sdk";
+//const socket = io(procces.env.REACT_APP_SERVER);
+const socket = io("http://localhost:4000");
+const Polly = new AWS.Polly(
+  {
+    accessKeyId: process.env.REACT_APP_KEY,
+    secretAccessKey: process.env.REACT_APP_SECRET,
+    region: "eu-west-3",
+  },
+  (data) => {
+    console.log(data);
+  }
+);
+class App extends Component {
+  componentDidMount() {
+    this.listenToServer();
+  }
+  alertsArray = [];
+  listenToServer = () => {
+    socket.on("event", (data) => {
+      console.log(data);
+      if (this.state.alert.length > 0) {
+        this.alertsArray.push(data);
+      } else {
+        this.setState({ alert: [data] });
+      }
+    });
+  };
+  alertPlaying = false;
+  speak = () => {
+    if (this.state.alert.length !== 0) {
+      console.log("ok");
+      this.alertPlaying = true;
+      Polly.synthesizeSpeech(
+        {
+          Text:
+            this.state.alert[0].user +
+            " has redeemed " +
+            this.state.alert[0].event,
+          TextType: "text",
+          VoiceId: "Brian",
+          OutputFormat: "mp3",
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+          } else if (data) {
+            let uInt8Array = new Uint8Array(data.AudioStream);
+            let arrayBuffer = uInt8Array.buffer;
+            let blob = new Blob([arrayBuffer]);
+            let url = URL.createObjectURL(blob);
+            console.log(url);
+            let textToSpeech = new Audio(url);
+            textToSpeech.volume = 0.4;
+            textToSpeech.play();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            console.log(this.state);
+          }
+        }
+      );
+    }
+  };
+  alertPlay = () => {
+    console.log("oks");
+    let sound = new Audio(trumpet);
+    sound.volume = 0.1;
+    sound.play();
+    setTimeout(() => {
+      this.speak();
+    }, 5000);
+    setTimeout(() => {
+      this.setState((prevState) => {
+        let alerts = Array.from(prevState);
+        alerts.shift();
+        console.log(this.alertsArray);
+        alerts = alerts.concat(this.alertsArray);
+        this.alertsArray = [];
+        console.log(alerts);
+        return { alert: alerts };
+      });
+    }, 10000);
+  };
+  state = { alert: [], alertPlaying: false };
+  render() {
+    if (this.state.alert.length === 0) {
+      return <div></div>;
+    }
+    console.log(this.state);
+    return (
+      <div>
+        <h2
+          style={{
+            background: "grey",
+            marginTop: 100,
+            textAlign: "center",
+            fontSize: 40,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.alert[0].user} has redeemed {this.state.alert[0].event}
+        </h2>
+        <audio>{this.alertPlay()}</audio>
+      </div>
+    );
+  }
 }
 
 export default App;
