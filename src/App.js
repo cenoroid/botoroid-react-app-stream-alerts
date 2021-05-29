4710e050c63986d708b0e5c6600dd9aed92b5b25
Cenoroid <almostwar@gmail.com>
2021-05-29 15:35:23 +0300

    1

--- src/App.js
+++ src/App.js
@@ -58,13 +58,13 @@ const App = () => {
       var blob = new Blob([arrayBuffer]);
       var url = URL.createObjectURL(blob);
       let textToSpeech = new Audio(url);
-      textToSpeech.volume = 0.4;
+      textToSpeech.volume = 0.7;
       textToSpeech.play();
     }
   }
   function alertPlay() {
     let sound = new Audio(trumpet);
-    sound.volume = 0.4;
+    sound.volume = 0.6;
     sound.play();
     setTimeout(() => {
       speak();
